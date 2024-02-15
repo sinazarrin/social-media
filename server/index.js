4f89964b2d0765c8c94b9dc9138de60ad1ea3a1a
sinazarrin <goldmontain8@gmail.com>
2024-02-15 12:12:26 +0330

    mongodb setup

--- server/index.js
+++ server/index.js
@@ -1,15 +1,15 @@
-import express from 'express'
-import bodyParser from 'body-parser'
-import mongoose from 'mongoose'
-import cors from 'cors'
-import dotenv from 'dotenv'
-import multer from 'multer'
-import helmet from 'helmet'
-import morgan from 'morgan'
-import path from 'path'
-import { fileURLToPath } from 'url'
+import express from "express";
+import bodyParser from "body-parser";
+import mongoose from "mongoose";
+import cors from "cors";
+import dotenv from "dotenv";
+import multer from "multer";
+import helmet from "helmet";
+import morgan from "morgan";
+import path from "path";
+import { fileURLToPath } from "url";
 
-// CONFIGURATIONS
+/* CONFIGURATIONS */
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 dotenv.config();
@@ -23,13 +23,26 @@ app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
 app.use(cors());
 app.use("/assets", express.static(path.join(__dirname, "public/assets")));
 
-/* FILE STORAGE */
+dotenv.config()
+
+// FILE STORAGE
 const storage = multer.diskStorage({
-    destination: function (req, file, cb) {
-        cb(null, "public/assets");
-    },
-    filename: function (req, file, cb) {
-        cb(null, file.originalname);
-    },
+  destination: function (req, file, cb) {
+    cb(null, "public/assets");
+  },
+  filename: function (req, file, cb) {
+    cb(null, file.originalname);
+  },
 });
 const upload = multer({ storage });
+
+// MONGOOSE SETUP
+const PORT = process.env.PORT || 6000
+mongoose.connect(process.env.MONGO_URL, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+})
+.then(() => {
+  app.listen(PORT, () => console.log(`Server Port: ${PORT}`))
+})
+.catch((error) => console.log(`${error} did no connect`))
